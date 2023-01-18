6502576fcc5a8e2e6f5139240c400930d1648311
alexandersimmons0 <122643996+alexandersimmons0@users.noreply.github.com>
2023-01-18 10:30:57 -0700

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -7,26 +7,26 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     // Update is called once per frame
+    public float speed = 20f;
     void Update()
     {
-        Vector3 pos = transform.position
-        public float speed = 20f;
-        if(Input.GetKey(KeyCode.w)){ //I dont know anything about C# but this should do something
-            pos.z += speed * Time.deltaTime;
+        Vector3 pos = transform.position;    
+        if (Input.GetKey(KeyCode.W)){ //I dont know anything about C# but this should do something
+            pos.z += speed* Time.deltaTime;
         }
-        if(Input.GetKey(KeyCode.s)){
-            pos.z -= speed * Time.deltaTime
+        if (Input.GetKey(KeyCode.S)){
+            pos.z -= speed * Time.deltaTime;
         }
-        if(Input.GetKey(KeyCode.a)){
-            pos.x += speed * Time.deltaTime;
-        }
-        if(Input.Getkey(KeyCode.d)){
+        if (Input.GetKey(KeyCode.A)){
             pos.x -= speed * Time.deltaTime;
         }
-        // either KeyCode.w or Input.GetKey("W") not sure yet
+        if (Input.GetKey(KeyCode.D)){
+            pos.x += speed * Time.deltaTime;
+        }
+// either KeyCode.w or Input.GetKey("W") not sure yet
         transform.position = pos;
     }
 }
